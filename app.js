88581e1156fd3c07499aadc27518648a57cb91dd
ShivamPersonal <agarwalshivamrbmhs@gmail.com>
2022-08-27 05:01:41 +0530

    Input and show output using innerText

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 var btnTranslate = document.querySelector("#btn-translate")
 var txtInput = document.querySelector("#txt-input-ar")
+var txtOutput = document.querySelector("#output")
 
 function log_btn_translate_event() {
     console.log("Btn clicked")
     console.log(txtInput.value)
+    txtOutput.innerText = txtInput.value;
 }
 
 btnTranslate.addEventListener("click", log_btn_translate_event)
+
