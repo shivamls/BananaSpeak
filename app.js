a402ab677bcb0706565f29def68976bb46b4824e
ShivamPersonal <agarwalshivamrbmhs@gmail.com>
2022-09-23 20:10:13 +0530

    Completing API calls

--- app.js
+++ app.js
@@ -6,7 +6,17 @@ function log_btn_translate_event() {
     console.log("Btn clicked")
     console.log(txtInput.value)
     txtOutput.innerText = txtInput.value;
+
+    baseUrl = "https://api.funtranslations.com/translate/minion.json"
+    //baseUrl = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json"
+    finalUrl = baseUrl + "?text=" + txtInput.value
+
+    var res_json = fetch(finalUrl)
+    
+    res_json.then(response => response.json()).then(json => {
+        var translatedText = json.contents.translated;
+        txtOutput.innerText = translatedText;
+    })
 }
 
 btnTranslate.addEventListener("click", log_btn_translate_event)
-
